26503fb363117548601d524bf9a6950ecbdfa06e
Felipe Calegari Garcia <felipe.phellipe@gmail.com>
2024-05-22 16:37:28 +0100

    Update DivinePrideInfo.java

--- src/main/java/com/alticelabs/ccp/exagon/divine_pride/info/DivinePrideInfo.java
+++ src/main/java/com/alticelabs/ccp/exagon/divine_pride/info/DivinePrideInfo.java
@@ -3,7 +3,6 @@ package com.alticelabs.ccp.exagon.divine_pride.info;
 
 public class DivinePrideInfo {
 
-    public static final String TOKEN = "637f41a2a818404bba5fa0e6f4132c82";
 
     public static final String apiKey = "?apiKey=" + TOKEN;
 
